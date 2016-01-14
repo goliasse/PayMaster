9903c292ac6a5ef6d6d52db3f4cc67139001f322
Sandeepan Kundu <sandeepan.kundu@gmail.com>
2016-01-13 20:25:07 -0500

    SK - IPreInsertEventListener  and IPreUpdateEventListener  handling placeholder for all changes

--- Paymaster/App_Start/DBPlumbing.cs
+++ Paymaster/App_Start/DBPlumbing.cs
@@ -1,7 +1,9 @@
-﻿using FluentNHibernate.Cfg;
+﻿using System;
+using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using FluentNHibernate.Conventions.Helpers;
 using NHibernate;
+using NHibernate.Event;
 using Paymaster.Model;
 
 namespace Paymaster.App_Start
@@ -16,10 +18,48 @@ namespace Paymaster.App_Start
                 .Mappings(m => m
                 .FluentMappings.AddFromAssemblyOf<Employees>()
                 .Conventions.Add(Table.Is((t => t.TableName.ToLower())))
-                )
+                ).
+                ExposeConfiguration(val =>
+                {
+                    val.AppendListeners(ListenerType.PreInsert, new IPreInsertEventListener[] { new PaymasterPreInsertEventListener() });
+                    val.AppendListeners(ListenerType.PreUpdate, new IPreUpdateEventListener[] { new PaymasterPreUpdateEventListener() });
+                })
                 .BuildSessionFactory();
 
             return isessionFactory;
+        }
+    }
+
+    public class PaymasterPreInsertEventListener : IPreInsertEventListener
+    {
+        public bool OnPreInsert(PreInsertEvent @event)
+        {
+            //var modelBase = @event.Entity as ModelBase;
+
+            //if (modelBase != null)
+            //{
+            //    var currentUtcTime = DateTime.UtcNow;
+            //    modelBase.AddedDateTime = currentUtcTime;
+            //    modelBase.ModifiedDateTime = currentUtcTime;
+            //    Set(@event.Persister, @event.State, "AddedDateTime", currentUtcTime);
+            //    Set(@event.Persister, @event.State, "ModifiedDateTime", currentUtcTime);
+            //}
+            return false;
+        }
+    }
+    public class PaymasterPreUpdateEventListener : IPreUpdateEventListener
+    {
+        public bool OnPreUpdate(PreUpdateEvent @event)
+        {
+            //var modelBase = @event.Entity as ModelBase;
+
+            //if (modelBase != null)
+            //{
+            //    var currentUtcTime = DateTime.UtcNow;
+            //    modelBase.ModifiedDateTime = currentUtcTime;
+            //    Set(@event.Persister, @event.State, "ModifiedDateTime", currentUtcTime);
+            //}
+            return false;
         }
     }
 }
